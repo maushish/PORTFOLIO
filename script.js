cacdf2f95b78aab4a039540f9083f63a86362141
Maushish Yadav <114429859+maushish@users.noreply.github.com>
2023-01-19 15:03:23 +0530

    Uploading all file to git hub
    only home section

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.menubtn').click(function(){
+        $('.navbar .menu').toggleClass("active")
+        $('.menubtn i').toggleClass("active")
+
+    });
+    
+
+});
+
+
+
+
+
